04dda0178ec5991f897196bd36aafe319a8d7d93
RalfZhang <zhangkang103@gmail.com>
2023-08-16 14:59:50 +0800

    fix: clear input error

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -20,9 +20,13 @@ export const Input = () => {
         options={countries.map(e => ({ code: e.code, label: e.name }))}
         openOnFocus={false}
         renderInput={(params) => (
-          <div ref={params.InputProps.ref}>
-            <InputBase type="text" sx={{ p: '0 16px', height: '50px' }} fullWidth {...params.inputProps} />
-          </div>
+          <InputBase
+            ref={params.InputProps.ref}
+            type="text"
+            sx={{ p: '0 16px', height: '50px' }}
+            fullWidth
+            inputProps={params.inputProps}
+          />
         )}
         sx={{ flexGrow: 1, m: '-4px 0' }}
       />
@@ -31,4 +35,4 @@ export const Input = () => {
       </IconButton>
     </Paper>
   )
-}
+};
